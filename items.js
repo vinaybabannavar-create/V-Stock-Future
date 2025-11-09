63187d1ddb894f31741b897ad67cd43103803b5b
vinaybabannavar <vinaybabannavar@gmail.com>
2025-11-09 17:21:21 +0530

    Initial project commit

---
+++ items.js
@@ -0,0 +1,82 @@
+const items = [
+  {
+    id: 1,
+    company: "NIFTY 50",
+    item_name: "NIFTY Index Fund",
+    image: "https://www.etmoney.com/learn/wp-content/uploads/2023/01/Nifty-50.png",
+    current_price: 2000,
+    original_price: 2400,
+    discount_percentage: 17,
+    rating: { stars: 4.5, count: 250 }
+  },
+  {
+    id: 2,
+    company: "Reliance",
+    item_name: "Reliance Industries Ltd.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo21zeAYniJgxKT7Nf9jeZ01VbkhpkolrbgA&s",
+    current_price: 2500,
+    original_price: 2900,
+    discount_percentage: 14,
+    rating: { stars: 4.8, count: 340 }
+  },
+  {
+    id: 3,
+    company: "ICICI Bank",
+    item_name: "ICICI Bluechip Fund",
+    image: "https://akm-img-a-in.tosshub.com/indiatoday/images/breaking_news/202402/ICICI%2520Bank_12.jpg?VersionId=D9ZOhefU01E7AGVpOdVgykXsDOiMV16W",
+    current_price: 1200,
+    original_price: 1500,
+    discount_percentage: 20,
+    rating: { stars: 4.3, count: 180 }
+  },
+  {
+    id: 4,
+    company: "HDFC",
+    item_name: "HDFC Securities",
+    image: "https://img.etimg.com/thumb/width-1600,height-900,imgsize-36958,resizemode-75,msid-123514532/markets/stocks/news/hdfc-bank-shares-in-focus-as-stock-to-trade-ex-bonus-for-11-issue.jpg",
+    current_price: 1500,
+    original_price: 1900,
+    discount_percentage: 21,
+    rating: { stars: 4.6, count: 210 }
+  },
+  {
+    id: 5,
+    company: "Infosys",
+    item_name: "Infosys IT Stock",
+    image: "https://akm-img-a-in.tosshub.com/indiatoday/images/breaking_news/202402/Infosys.jpg%3FVersionId%3DHRtTL6C78uQGA5mk4yrabvJZTm.p6dyd?VersionId=zA89rQxSlXLYYJo2X5XB0zZsNwgjtSCc",
+    current_price: 1800,
+    original_price: 2100,
+    discount_percentage: 14,
+    rating: { stars: 4.7, count: 330 }
+  },
+  {
+    id: 6,
+    company: "Tata Motors",
+    item_name: "Tata Electric Mobility",
+    image: "https://c8.alamy.com/comp/2FMJ1DM/tata-motors-company-logo-on-a-website-with-blurry-stock-market-developments-in-the-background-seen-on-a-computer-screen-through-a-magnifying-glass-2FMJ1DM.jpg",
+    current_price: 1300,
+    original_price: 1500,
+    discount_percentage: 13,
+    rating: { stars: 4.2, count: 190 }
+  },
+  {
+    id: 7,
+    company: "SBI Bank",
+    item_name: "SBI Advantage Fund",
+    image: "https://www.shutterstock.com/image-photo/sbi-logo-displayed-on-smartphone-260nw-2648984229.jpg",
+    current_price: 1600,
+    original_price: 1900,
+    discount_percentage: 16,
+    rating: { stars: 4.4, count: 250 }
+  },
+  {
+    id: 8,
+    company: "Adani Power",
+    item_name: "Adani Renewable Energy",
+    image: "https://www.shutterstock.com/image-photo/jakarta-february-072023-adani-group-600nw-2258793101.jpg",
+    current_price: 2200,
+    original_price: 2500,
+    discount_percentage: 12,
+    rating: { stars: 4.5, count: 280 }
+  }
+];
